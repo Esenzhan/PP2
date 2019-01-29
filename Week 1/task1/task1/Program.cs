c66fd4742a3c0cd42add50829573bf446fc12f9c
Esenzhan Nurbaev <esenzhan.nur@gmail.com>
2019-01-30 00:09:23 +0600

    task1 is done

---
+++ Week 1/task1/task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace task1
+{
+
+    class Program
+    {
+        //пишу функцию для проверки чисел на prime
+        public static bool isprime(int b)
+        {
+            if (b == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i * i <= b; ++i)
+            {
+                if (b % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static void Main(string[] args)
+        {
+            //создаю переменную и сохраняю в него количество чисел
+            int n = int.Parse(Console.ReadLine());
+            //создаю массив строк, считываю с консоли n чисел и разделяю их на каждый элемент массива
+            string[] arr = Console.ReadLine().Split();
+            //создаю массив чисел
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                //перевожу элементы string в int и сохраняю в массив
+                a[i] = int.Parse(arr[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                //проверяю и вывожу простые числа
+                if (isprime(a[i]) == true)
+                    Console.Write(a[i] + " ");
+            }
+            //ожидание нажатия клавиши
+            Console.ReadKey();
+        }
+    }
+
+}
