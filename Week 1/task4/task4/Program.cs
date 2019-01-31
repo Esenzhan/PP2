fb73f1f3ed8cbea90919f512c14e93a366d4d3b9
Esenzhan Nurbaev <esenzhan.nur@gmail.com>
2019-01-31 08:50:39 +0600

    task4 is done

---
+++ Week 1/task4/task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создаю переменную и сохраняю в него размер массива
+            int n = int.Parse(Console.ReadLine());
+            //создаю 2d массив string
+            string[,] arr = new string[n,n];
+            //заполняю массив строчками "[*]"
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0;j < n; j++){
+                    arr[i, j] +="[*]";
+            }
+            }
+            //вывожу на консоль только нужные мне звезды
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if(i>=j)
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
