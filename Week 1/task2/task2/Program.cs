505ebf795cc5185178321d4bbceb54cea0c2a6a0
Esenzhan Nurbaev <esenzhan.nur@gmail.com>
2019-02-02 01:11:07 +0600

    task2 is done

---
+++ Week 1/task2/task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year;
+        //создаю конструктор с 2 параметрами
+        public Student(string name,string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        //создаю метод
+        public void print(int year)
+        {
+            Console.WriteLine("name:          " + name);
+            Console.WriteLine("ID:            " + id);
+            Console.WriteLine("Year of study: " + (++year));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string i = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            Student st1 = new Student(s,i);
+            st1.print(n);
+            Console.ReadKey();
+        }
+    }
+}
