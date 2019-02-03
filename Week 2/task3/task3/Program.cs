dd038ccced767e77365fb5d70d3537a5c12fac2a
Esenzhan Nurbaev <esenzhan.nur@gmail.com>
2019-02-03 19:53:30 +0600

    task3 of the 2nd week is done

---
+++ Week 2/task3/task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace task3
+{
+    class Program
+    {
+        //создаю функцию для создания табов
+        public static void PrintSpaces(int n){
+        for(int i = 0; i < n; i++)
+            {
+                Console.Write("      ");
+            }
+        }
+        //создаю функцию для выведения на консоль всех папок и файлов по заданному пути
+        public static void Director(DirectoryInfo dir,int a)
+        {
+            foreach(FileInfo f in dir.GetFiles()){
+                PrintSpaces(a);
+                Console.WriteLine(f.Name);
+            }
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                PrintSpaces(a);
+                Console.WriteLine(d.Name);
+              
+                Director(d, a+1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo df = new DirectoryInfo(@"C:\Users\123\Desktop\C##");
+            int b = 0;
+            Director(df, b);
+            Console.ReadKey();
+        }
+    }
+}
