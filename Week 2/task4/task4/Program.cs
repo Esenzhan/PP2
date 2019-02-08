40a3fdb27496861eaab7395c293e0606db336c5b
Esenzhan Nurbaev <esenzhan.nur@gmail.com>
2019-02-09 00:01:04 +0600

    task4 of the 2nd week is done

---
+++ Week 2/task4/task4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathString = Path.Combine(@"C:\path");
+            string fileName = "example.txt";
+            pathString = Path.Combine(pathString, fileName);
+            FileStream fs = File.Create(pathString);
+            //Закрываю поток
+            fs.Close();
+
+
+            //Источник
+            string sourcePath = @"C:\path";
+            // Цель
+            string targetPath = @"C:\path1";
+            string sourceFile = Path.Combine(sourcePath, fileName);
+            string destFile = Path.Combine(targetPath, fileName);
+
+            Directory.CreateDirectory(targetPath);
+
+            // Чтобы скопировать файл в другое место 
+            // и перезаписать файл назначения, если он уже существует.
+
+            File.Copy(sourceFile, destFile, true);
+
+
+            if (File.Exists(@"C:\path\example.txt"))
+            {
+                try
+                {
+                    File.Delete(@"C:\path\example.txt");
+                }
+                catch (IOException a)
+                {
+                    Console.WriteLine(a.Message);
+                    return;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
